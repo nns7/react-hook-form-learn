e4684ff37ea8b6ed3bbd32cc2bfab51374686d8f
nns7 <nns7snn@gmail.com>
2022-12-21 11:43:32 +0900

    :+1: エラーメッセージの設定。

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import { FieldValues, useForm } from "react-hook-form";
 
+type Login = {
+  email: string;
+  password: string;
+};
+
 function App() {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<Login>();
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
@@ -16,8 +21,11 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="email">Email</label>
-          <input id="email" {...register("email", { required: true })} />
-          {errors.email && <div>入力が必須の項目です</div>}
+          <input
+            id="email"
+            {...register("email", { required: "入力が必須の項目です。" })}
+          />
+          {errors.email?.message && <div>{errors.email.message}</div>}
         </div>
         <div>
           <label htmlFor="password">Password</label>
