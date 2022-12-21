4edb955c40268117dd118ad08da0a64c54b418f4
nns7 <nns7snn@gmail.com>
2022-12-21 18:04:37 +0900

    :+1: バリデーションライブラリYupの利用。

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,35 @@
 import React from "react";
 import "./App.css";
-import { FieldValues, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-type Login = {
-  email: string;
-  password: string;
-  showAge: boolean;
-  age: number;
-};
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("メールアドレスの形式ではありません。")
+    .required("入力が必須の項目です。"),
+  password: yup
+    .string()
+    .min(8, "8文字以上入力してください。")
+    .required("入力が必須の項目です。"),
+  showAge: yup.boolean(),
+  age: yup.number().min(50, "50以上を入力してください。"),
+});
+
+type Login = yup.InferType<typeof schema>;
 
 function App() {
   const {
     register,
     handleSubmit,
-    formState: { errors, isDirty, isValid },
+    formState: { errors },
     watch,
   } = useForm<Login>({
-    criteriaMode: "all",
-    defaultValues: { email: "", password: "" },
-    mode: "onChange",
+    resolver: yupResolver(schema),
   });
   const watchShowAge = watch("showAge", false);
-  const onSubmit = (data: FieldValues) => console.log(data);
+  const onSubmit = (data: Login) => console.log(data);
 
   return (
     <div className="App">
@@ -29,35 +37,13 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label htmlFor="email">Email</label>
-          <input
-            id="email"
-            {...register("email", { required: "入力が必須の項目です。" })}
-          />
-          {errors.email?.message && <div>{errors.email.message}</div>}
+          <input id="email" {...register("email", { required: true })} />
+          <p>{errors.email?.message}</p>
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            {...register("password", {
-              required: { value: true, message: "入力が必須の項目です。" },
-              pattern: {
-                value: new RegExp(/^[A-Za-z]+$/),
-                message: "アルファベットのみ入力してください。",
-              },
-              minLength: { value: 8, message: "8文字以上入力してください。" },
-            })}
-            type="password"
-          />
-          {errors.password?.types?.required && (
-            <div>{errors.password.types.required}</div>
-          )}
-          {errors.password?.types?.minLength && (
-            <div>{errors.password.types.minLength}</div>
-          )}
-          {errors.password?.types?.pattern && (
-            <div>{errors.password.types.pattern}</div>
-          )}
+          <input id="password" {...register("password")} type="password" />
+          <p>{errors.password?.message}</p>
         </div>
         <div>
           <label htmlFor="age">Age</label>
@@ -67,18 +53,11 @@ function App() {
         </div>
         {watchShowAge && (
           <div>
-            <input
-              type="number"
-              {...register("age", {
-                min: { value: 50, message: "50以上を入力してください。" },
-              })}
-            />
-            {errors.age?.message && <div>{errors.age.message}</div>}
+            <input type="number" {...register("age")} />
+            <p>{errors.age?.message}</p>
           </div>
         )}
-        <button type="submit" disabled={!isDirty || !isValid}>
-          ログイン
-        </button>
+        <button type="submit">ログイン</button>
       </form>
     </div>
   );
